8718c100274b45462d56cbf56250f12e4bf32f9b
bowei <bowei.han100@gmail.com>
2018-12-01 17:34:37 -0500

    add and remove sheets

---
+++ src/main/java/com/xl365vcpoi/api/compare/NaiveWorkbookCompare.java
@@ -0,0 +1,38 @@
+package com.xl365vcpoi.api.compare;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.google.common.collect.Sets;
+import com.xl365vcpoi.api.service.ExcelWorkbookUtility;
+
+@Component
+public class NaiveWorkbookCompare implements ExcelCompareStrategy {
+
+	@Autowired
+	private ExcelWorkbookUtility excelWorkbookUtility;
+	
+	@Override
+	public ExcelCompareResult compare(XSSFWorkbook source, XSSFWorkbook copy) {
+		Set<String> sourceSheets = excelWorkbookUtility.getSheetNames(source);
+		Set<String> copySheets = excelWorkbookUtility.getSheetNames(copy);
+		
+		Set<String> removedSheets = Sets.difference(sourceSheets, copySheets);
+		Set<String> addedSheets = Sets.difference(copySheets, sourceSheets);
+		
+		// TODO: refactor ExcelCompareResult
+		List<String> sheetDifferences = new ArrayList<>();
+		removedSheets.forEach(sheet -> {
+			sheetDifferences.add("Removed: " + sheet);
+		});
+		addedSheets.forEach(sheet -> {
+			sheetDifferences.add("Added: " + sheet);
+		});
+		return new ExcelCompareResult(sheetDifferences);
+	}
+}
--- src/main/java/com/xl365vcpoi/api/service/ExcelCompareService.java
+++ src/main/java/com/xl365vcpoi/api/service/ExcelCompareService.java
@@ -1,18 +1,22 @@
 package com.xl365vcpoi.api.service;
 
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.xl365vcpoi.api.compare.ExcelCompareContext;
 import com.xl365vcpoi.api.compare.ExcelCompareResult;
-import com.xl365vcpoi.api.compare.NaiveCellCompare;
+import com.xl365vcpoi.api.compare.NaiveWorkbookCompare;
 import com.xl365vcpoi.api.helper.Pair;
 
 @Service
 public class ExcelCompareService {
 
+	@Autowired
+	private NaiveWorkbookCompare naiveWorkbookCompare;
+	
 	public ExcelCompareResult compareFiles(Pair<XSSFWorkbook, XSSFWorkbook> excelFilePair) {
-		ExcelCompareContext context = new ExcelCompareContext(new NaiveCellCompare());
+		ExcelCompareContext context = new ExcelCompareContext(naiveWorkbookCompare);
 		return context.executeStrategy(excelFilePair.first, excelFilePair.second);
 	}
 
